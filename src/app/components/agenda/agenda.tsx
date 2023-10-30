038cddc6079452975a315f4f74d8084b0b674a33
Rafael Amaral <59116502+Rafael-F-Amaral@users.noreply.github.com>
2023-10-30 14:17:49 -0300

    30/10/2023-14h-17m
    Criação de uma table e implementação de input com estilização em javascript das borders

--- src/app/components/agenda/agenda.tsx
+++ src/app/components/agenda/agenda.tsx
@@ -2,65 +2,115 @@
 
 import React, { useState } from 'react';
 import styles from './agenda.module.css'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
-type TabProps = {
-    label: string;
-    content: React.ReactNode;
-};
 
-const Tab: React.FC<TabProps> = ({ label, content }) => {
+
+
+
+function FillExample() {
+
+    const [tableData, setTableData] = useState([
+        ['1', 'Inserir treinamento', 'Outro Dado 1', 'Outro Dado 1', 'Outro Dado 1', 'Outro Dado 1', 'Outro Dado 1', 'Outro Dado 1'],
+        ['Nome 2', 'Idade 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2'],
+        ['Nome 2', 'Idade 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2'],
+        ['Nome 2', 'Idade 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2'],
+        ['Nome 2', 'Idade 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2', 'Outro Dado 2'],
+    ]);
+
+    const handleCellChange = (rowIndex: number, colIndex: number, value: string) => {
+        const updatedData = [...tableData];
+        updatedData[rowIndex][colIndex] = value;
+        setTableData(updatedData);
+    };
+
+    const applyTesteClass = (content: string) => {
+        const cellContent = parseInt(content);
+        return !isNaN(cellContent) && cellContent > 5 ? 'Teste' : '';
+    };
+
+
+
     return (
-        <div>
-            <h2>{label}</h2>
-            {content}
-        </div>
-    );
-};
-
-type TabsProps = {
-    tabs: TabProps[];
-};
-
-const Tabs: React.FC<TabsProps> = ({ tabs }) => {
-    const [activeTab, setActiveTab] = useState(0);
-
-    return (
-        <div>
-            <div className="tab-labels">
-                {tabs.map((tab, index) => (
-                    <button
-                        key={index}
-                        onClick={() => setActiveTab(index)}
-                        className={activeTab === index ? 'active' : ''}
+        <div
+            className=
+            {styles.main}
+        >
+            <div>
+                <Tabs
+                    defaultActiveKey="home"
+                    id="fill-tab-example"
+                    className={`mb-3 ${styles.mainTab}`}
+                    fill
+                >
+                    <Tab
+                        eventKey="home"
+                        title="Home"
+                        className={styles.contentTab}
                     >
-                        {tab.label}
-                    </button>
-                ))}
-            </div>
-            <div className="tab-content">
-                {tabs[activeTab].content}
+                        <div className={styles.tableHead}>
+                            <table>
+                                <thead>
+                                    <tr className={styles.thCell}>
+                                        <th></th>
+                                        <th>SEGUNDA-FEIRA</th>
+                                        <th>TERÇA-FEIRA</th>
+                                        <th>QUARTA-FEIRA</th>
+                                        <th>QUINTA-FEIRA</th>
+                                        <th>SEXTA-FEIRA</th>
+                                        <th>SÁBADO</th>
+                                        <th
+                                            className={styles.thCellBorder}>
+                                            DOMINGO
+                                        </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {tableData.map((row, rowIndex) => (
+                                        <tr key={rowIndex} className={styles.trCell}>
+                                            {row.map((cellValue, colIndex) => (
+                                                <td
+                                                    key={colIndex}
+                                                    className={`${colIndex === 7 ? styles.Teste : ''} ${rowIndex === 4 ? styles.Teste2 : ''} ${styles.tdCell}`}
+                                                >
+                                                    <input
+                                                        type="text"
+                                                        value={cellValue}
+                                                        onChange={(e) =>
+                                                            handleCellChange(rowIndex, colIndex, e.target.value)
+                                                        }
+                                                    />
+                                                </td>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table></div>
+                    </Tab>
+                    <Tab
+                        eventKey="profile"
+                        title="Profile"
+                    >
+                        Tab content for Profile
+                    </Tab>
+                    <Tab
+                        eventKey="longer-tab"
+                        title="Loooonger Tab"
+                    >
+                        Tab content for Loooonger Tab
+                    </Tab>
+                    <Tab
+                        eventKey="contact"
+                        title="Contact"
+                    >
+                        Tab content for Contact
+                    </Tab>
+                </Tabs>
             </div>
         </div>
     );
-};
+}
 
-// Exemplo de uso:
-const App: React.FC = () => {
-    const tabs: TabProps[] = [
-        { label: 'Tab 1', content: <p>Conteúdo da Tab 1</p> },
-        { label: 'Tab 2', content: <p>Conteúdo da Tab 2</p> },
-        { label: 'Tab 3', content: <p>Conteúdo da Tab 3</p> },
-    ];
-
-    return (
-        <div className={styles.mainAgenda}>
-            <h1>Componente de Abas</h1>
-            <div className={styles.tabsMain}>
-                <Tabs tabs={tabs} />
-
-            </div>
-        </div>
-    );
-};
-
-export default App;
+export default FillExample;
