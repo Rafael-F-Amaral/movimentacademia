3615bfd8940ac3b08a8355c8fb4d05ee0091ff39
Rafael Amaral <59116502+Rafael-F-Amaral@users.noreply.github.com>
2023-11-01 11:13:46 -0300

    01/11/2023-11-13h
    Criação do html do resto da página

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    Mapa
+                </div>
+                <div>
+                    <p><h2>Como chegar:</h2></p>
+                    <p>R: João da Dadona, 252</p>
+                    <p>Iracemápolis, SP 13495-070 </p>
+                    <span>
+                        <p>Telefone/Whatsapp</p>
+                        <p>
+                            <img
+                                src="/moviment-academia-assets/footer/telefone.svg"
+                                alt="telefone para contato com a academia moviment academia de iracemápolis"
+                            />
+                            (19) 99248-2760</p>
+                        <p>
+                            <img
+                                src="/moviment-academia-assets/footer/telefone.svg"
+                                alt="telefone para contato com a academia moviment academia de iracemápolis"
+                            />
+                            (19) 98213-0600</p>
+
+                    </span>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <p><h2>Horário de Funcionamento:</h2></p>
+                    <p>Segunda à sexta-feira</p>
+                    <p>5:20 às 12:00 / 14:00 às 21:00</p>
+                    <div>
+                        <span>
+                            <p>sábado</p>
+                            <p> 8:00 ÀS 11:00</p>
+                        </span>
+                        <span>
+                            <p>Domingo</p>
+                            <p>FECHADO</p>
+                        </span>
+                    </div>
+                    <span>
+                        <p>SIGA NOSSAS REDES</p>
+                        <img
+                            src="/moviment-academia-assets/footer/telefone.svg"
+                            alt="Rede social instagram da academia moviment academia de Iracemápolis"
+                        />
+                        <img
+                            src="/moviment-academia-assets/footer/telefone.svg"
+                            alt="Rede social facebook da academia moviment academia de Iracemápolis"
+                        />
+                    </span>
+                </div>
+                <span>Copyright</span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/app/components/agenda/agenda.tsx
+++ src/app/components/agenda/agenda.tsx
@@ -3,7 +3,9 @@
 import React, { useState } from 'react';
 import styles from './agenda.module.css'
 import Tab from 'react-bootstrap/Tab';
+import TabPane from 'react-bootstrap/TabPane';
 import Tabs from 'react-bootstrap/Tabs';
+
 
 function FillExample() {
     const [tableData, setTableData] = useState([
@@ -18,10 +20,7 @@ function FillExample() {
         updatedData[rowIndex][colIndex] = value;
         setTableData(updatedData);
     };
-    const applyTesteClass = (content: string) => {
-        const cellContent = parseInt(content);
-        return !isNaN(cellContent) && cellContent > 5 ? 'Teste' : '';
-    };
+
     return (
         <div
             className=
@@ -29,12 +28,12 @@ function FillExample() {
         >
             <div>
                 <Tabs
-                    defaultActiveKey="imc"
+                    defaultActiveKey="CALORIAS"
                     id="fill-tab-example"
                     className={`mb-3 ${styles.mainTab}`}
                     fill
                 >
-                    <Tab
+                    <TabPane
                         eventKey="agenda"
                         title="AGENDA DE HORÁRIOS"
                         className={styles.contentTab}
@@ -76,24 +75,172 @@ function FillExample() {
                                         </tr>
                                     ))}
                                 </tbody>
-                            </table></div>
-                    </Tab>
-                    <Tab
+                            </table>
+                        </div>
+                    </TabPane>
+
+                    <TabPane
                         eventKey="imc"
                         title="CALCULAR IMC"
                     >
-                        Tab content for Profile
-                    </Tab>
-                    <Tab
+                        <div className={styles.mainIMC}>
+                            <div className={styles.contentImc}>
+                                <div>
+                                    <h2>SEU IMC</h2>
+                                    <table>
+                                        <tr>
+                                            <th>IMC</th>
+                                            <th>STATUS DO PESO</th>
+                                        </tr>
+                                        <tr>
+                                            <td>Abaixo de 19.5</td>
+                                            <td>Abaixo do peso</td>
+                                        </tr>
+                                        <tr>
+                                            <td>18,5 - 24,5</td>
+                                            <td>Saudável</td>
+                                        </tr>
+                                        <tr>
+                                            <td>25,0 - 29,9</td>
+                                            <td>Sobrepeso</td>
+                                        </tr>
+                                        <tr>
+                                            <td>30,0 e acima</td>
+                                            <td>Obeso</td>
+                                        </tr>
+                                        <caption><p>Taxa Metabólica Basal (TMB) / Índice de Massa Corporal (IMC) ? </p></caption>
+                                    </table>
+                                </div>
+
+                                <div>
+                                    <h3>Calcule seu IMC</h3>
+                                    <p>Ter um peso dentro do intervalo de peso ideal, ajuda a evitar o aparecimento de várias doenças crônicas, como diabetes e hipertensão.</p>
+
+                                    <table>
+                                        <tr>
+                                            <th>ALTURA(CM)</th>
+                                            <th>PESO/KG</th>
+                                        </tr>
+                                        <tr>
+                                            <td>175</td>
+                                            <td>90</td>
+                                        </tr>
+                                        <tr>
+                                            <th>IDADE</th>
+                                            <th>SEXO</th>
+
+                                        </tr>
+                                        <tr>
+                                            <td>35</td>
+                                            <td>HOMEM</td>
+                                        </tr>
+                                        <tr>
+                                            <td>30,0 e acima</td>
+                                            <td>Obeso</td>
+                                        </tr>
+                                        <tr>
+                                            <th>SELECIONE UM FATOR DE ATIVIDADE</th>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                POUCO OU NENHUM EXERCÍCIO/TRABALHO SENTADO
+                                            </td>
+
+                                        </tr>
+                                    </table>
+                                    <span>
+                                        <button>
+                                            CALCULAR
+                                        </button>
+                                    </span>
+                                </div>
+                                <div>
+                                    <img src="/moviment-academia-assets/agenda/imc/pessoa.svg" alt="imagem pessoa da academia moviment academia iracemápolis" />
+                                    <p>VOCÊ ESTÁ OBESO!</p>
+                                    <p>Seu ICM é de 31,62. Sua TMB está em 2.300 kcal/dia e TMB com seu fator  de atividade está em 2.800 kcal/dia ?</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </TabPane>
+
+                    <TabPane
                         eventKey="CALORIAS"
                         title="CALCULAR CALORIAS"
                     >
-                        Tab content for Loooonger Tab
-                    </Tab>
+                        <div className={styles.mainCalorias}>
+                            <div className={styles.resultadoContent}>
+                                <h3>Resultado:</h3>
+                                <p>Sua base de ingestão calórica diária é de 2504 kcal para chegar em seu objetivo.</p>
+                                <span>Linha horizontal</span>
+                                <p>Este é o numero de calorias diárias que deve ingerir para conseguir perder 35 kg, sem colocar a sua saúde em risco.</p>
+                                <p>À medida que vai emagrecendo e chegando mais perto do peso que deseja, deve recalcular o número de calorias que pode ingerir por dia para acelerar o processo de emagrecimento. Para isso, basta preencher novamente a calculadora com os seus dados atualizados.</p>
+                                <p>
+                                    <img
+                                        src="/moviment-academia-assets/agenda/calorias/caution.svg"
+                                        alt="Cuidado, aviso de cuidado para que as pessoas fiquem alertas a mensagem passada"
+                                    />
+                                    ATENÇÃO: é importante ressaltar que a quantidade de calorias também deve variar de acordo com o estado de saúde, metabolismo e produção de hormônios de cada pessoa.
+                                </p>
+                                <p>
+                                    Essa Calculadora não leva em consideração a realidade alimentar e a saúde dos indivíduos e não deve ser usada como única fonte de informação para sua saúde.
 
+                                </p>
+                                <p>
+                                    Por isso, para confirmar a quantidade diária de ingestão calórica é sempre aconselhada a consulta com um nutricionista.
+                                </p>
+                            </div>
+
+                            <div className={styles.formerContent}>
+                                <h3>
+                                    Calcular Calorias Diárias
+                                </h3>
+                                <p>
+                                    Calcular calorias diárias é uma forma bastante popular de se manter no peso, engordar ou emagrecer. Com essa calculadora é possível ter uma base de quantas calorias é preciso para chegar ao seu objetivo. ?
+                                </p>
+                                <div>
+                                    <div>
+                                        <h5>ALTURA</h5>
+                                        <p>175</p>
+                                        <h5>idade</h5>
+                                        <p>35</p>
+                                    </div>
+                                    <div>
+                                        <h5>PESO/KG</h5>
+                                        <p>90</p>
+                                        <h5>SEXO</h5>
+                                        <p>HOMEm</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <span>
+                                        <h5>SELECIONE SEU OBJETIVO</h5>
+                                        <p>QUERO EMAGRECER</p>
+                                    </span>
+                                    <span>
+                                        <h5>SELECIONE SEU TIPO DE EMPREGO</h5>
+                                        <p>POUCO OU NENHUM EXERCÍCIO/TRABALHO SENTADO</p>
+                                    </span>
+                                    <span>
+                                        <h5>SELECIONE SEU EXERCÍCIO SEMANAL:</h5>
+                                        <p>POUCO OU NENHUM EXERCÍCIO FÍSICO</p>
+                                    </span>
+                                    <div>
+                                        <button>
+                                            CALCULAR
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </TabPane>
                 </Tabs>
             </div>
+
         </div>
     );
 }
+
+
 export default FillExample;
